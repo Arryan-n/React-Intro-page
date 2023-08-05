17929c6c7f0f3f4a43abf6fb5d8deeb3192acb5f
Aryan baliy@n <ankushbaliyan142@gmail.com>
2023-08-05 14:49:09 +0530

    static react page

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import Header from "./Header";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+import Mainfile from "./Mainfile";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Navbar />
+      <Mainfile />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import pic from "./aryan.jpg";
+
+function Header() {
+  return (
+    <div>
+      <img src={pic} width="320rem" height="270rem" />
+    </div>
+  );
+}
+
+export default Header;
